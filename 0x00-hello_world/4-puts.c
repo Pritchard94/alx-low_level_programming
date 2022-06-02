2eabf9bd30b221733c789fa5f1873a556a273661
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-02 09:55:30 -0700

    vb

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 /**
  * main-th is the Entry Point
- * of the program
- * return : returns value of 0
+ * of the program.
+ *
+ * return - returns value of 0
  */
 int main(void)
 {	char String[] = "\"Programming is like building a multilingual puzzle";
 	puts(String);
 /**
- * return : this returns the value of the main function
+ * return - this returns the value of the main function
  */
 	return (0);
 }
