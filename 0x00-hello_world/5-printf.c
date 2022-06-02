cc182f704cded9f90b30377c7ea22ab09fc8f81c
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-02 10:24:30 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - this is the main function to print 
+ * 
+ * Return: the return value of the main function
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+/**
+ * Return: this is the return value of the main function
+ */
+	return (0);
+}
