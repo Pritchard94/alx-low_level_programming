9adb012f3f9a3834087d60d3f8f6b900fbc50dac
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-02 08:18:24 -0700

    qws

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+/**
+ * main - this is the entry point
+ */
 int main(void)
 {	/* writing string */
-	char Mystr[]="\"Programming is like building a multilingual puzzle"; 
+	char Mystr[] = "\"Programming is like building a multilingual puzzle"; 
 	/* Main : this is a file that*/
 	puts(Mystr);
        /* return value */	
-	return (0);
+	return(0);
 }
