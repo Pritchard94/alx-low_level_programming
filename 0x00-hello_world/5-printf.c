8fcda7ad09353644d9c3031e69d4f3854ea8d0cb
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-02 10:35:13 -0700

    dcf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 /**
- * main - this is the main function to print 
- * 
- * Return: the return value of the main function
+ * main _ this is the entry point
+ * printf function
+(*
+ * description: alx)?
+ *
+ * Return: return value of main.
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-/**
- * Return: this is the return value of the main function
- */
 	return (0);
 }
