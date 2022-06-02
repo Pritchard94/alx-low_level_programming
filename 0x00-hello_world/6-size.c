e8e5c86e768fe1a29d7fdd2e82c0c619f59ecbe4
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-02 16:09:00 -0700

    scx

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,18 @@
  */
 int main(void)
 {
-	int i;
-	float f;
-	char c;
-	long long int ll;
-	long int l;
+int i;
+float f;
+char c;
+long long int ll;
+long int l;
+/* print size of variables*/
+printf("Size of char: %ld byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of int: %ld byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of long int: %ld byte(s)\n", (unsigned long)sizeof(l));
+printf("Size of long long int: %ld byte(s)\n", (unsigned long)sizeof(ll));
+printf("Size of float: %ld byte(s)\n", (unsigned long)sizeof(f));
 
-	printf("Size of char: %ld byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of int: %ld byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of long int: %ld byte(s)\n", (unsigned long)sizeof(l));
-	printf("Size of long long int: %ld byte(s)\n", (unsigned long)sizeof(ll));
-	printf("Size of float: %ld byte(s)\n", (unsigned long)sizeof(f));
+return (0);
 
-	return (0);
 }
