1a1a6475427ceb120c1bbf54eb3011e35eb0853e
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-02 16:47:49 -0700

    jk

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,26 @@
 #include<stdio.h>
-
 /**
- * main - this is the entry point
- * good
+ * main -h this is the entry point.
  *
- * Return: the return value of 0 for success.
+ * Return: 0 (success)
  */
 int main(void)
 {
-int i;
-float f;
-char c;
-long long int ll;
-long int l;
+	char c;
+	int i;
+	long int l;
+	long long int ll;
+	float f;
+/*print variable sizes*/
+	printf("Size of char: %ld byte(s)\n", (unsigned long)sizeof(c));
 
-/* print size of variables*/
-printf("Size of char: %ld byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of int: %ld byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of long int: %ld byte(s)\n", (unsigned long)sizeof(l));
-printf("Size of long long int: %ld byte(s)\n", (unsigned long)sizeof(ll));
-printf("Size of float: %ld byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+	printf("Size of int: %ld byte(s)\n", (unsigned long)sizeof(i));
 
+	printf("Size of long int: %ld byte(s)\n", (unsigned long)sizeof(l));
+
+	printf("Size of long long int: %ld byte(s)\n", (unsigned long)sizeof(ll));
+
+	printf("Size of float: %ld byte(s)\n", (unsigned long)sizeof(f));
+
+	return (0);
 }
