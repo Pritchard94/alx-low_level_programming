ed616be57731d4b04042d20d9ee46113af1432ec
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-02 07:23:32 -0700

    commitr

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,17 @@
 
 #include <stdio.h>
 
-int main (void)
-{	/* writing string */
-	char Mystr[] = "Programming is like building a multilingual puzzle";
+int main(void)
 
-	puts(Mystr);
+{	/* writing string */
+
+	char Mystr[] = "\"Programming is like building a multilingual puzzle"; 
+	/** Main : this is a file that
+	 * outputs a message
+	 * in betty style
+	 */
+
+	puts(Mystr); 
 
 	return (0);
 }
