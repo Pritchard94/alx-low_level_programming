1a527dfcc030699a1422934cda52b909c4036048
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-02 09:51:09 -0700

    axc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 /**
  * main-th is the Entry Point
  * of the program
- * return - returns value of 0
+ * return : returns value of 0
  */
 int main(void)
 {	char String[] = "\"Programming is like building a multilingual puzzle";
 	puts(String);
 /**
- * return - this returns the value of the main function
+ * return : this returns the value of the main function
  */
 	return (0);
 }
