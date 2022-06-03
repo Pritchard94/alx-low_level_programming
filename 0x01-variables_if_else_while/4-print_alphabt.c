c1af7cac33ad5db8e93edf989380c45bf5e76a00
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-03 14:31:16 -0700

    axc

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - this is a program
+ * for alphabets
+ *
+ * Return: always
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);| grep -v [eq]
+	putchar('\n');
+
+	return(0);
+}
