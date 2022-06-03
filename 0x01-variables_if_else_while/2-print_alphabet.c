0f1e95b8a817ec223010d9353991129d34eb0f72
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-03 12:12:25 -0700

    dc

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main -
+ *
+ * Return: success
+ */
+int main(void)
+{	
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++);
+	putchar (ch);
+
+	return (0);
+}
