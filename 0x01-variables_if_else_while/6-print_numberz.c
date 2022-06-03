28f4f84ac0924adf8b4b314e55f53227313f0b5c
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-03 14:42:46 -0700

    df

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - this is a program
+ * for numbers
+ *
+ * Return: success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
