356e93be8744cb89efcbd8ae030d473b7ca8ddd6
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-03 16:30:52 -0700

    sc

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - 
+ *
+ * Return: success
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+		putchar(i);
+	putchar(',');
+	putchar(' ');
+	i++;
+
+	return (0);
+}
