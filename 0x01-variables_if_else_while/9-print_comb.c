e2053a2e0ad0e6755a27658fa3e95c2f6b1f3c60
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-03 17:33:47 -0700

    lp

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,10 @@ int main(void)
 
 	for (i = 0; i <= 9; i++) {
 		putchar(i);
-	if (i < 9)
-		putchar(44);
-	if (i < 9)
-		putchar(32);
+			if (i < 9)
+				putchar(44);
+			if (i < 9)
+				putchar(32);
 	}
 	
 	return (0);
