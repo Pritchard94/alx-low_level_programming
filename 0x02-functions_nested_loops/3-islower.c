fabacde618e75035eaf4b15e1245c9ec852664d8
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-07 16:37:23 -0700

    f

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - lower
+ *
+ * Return: 1 for lowercase character 0 if anything else
+ */
+int _islower(int c)
+{
+	if (c >= 97; && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
