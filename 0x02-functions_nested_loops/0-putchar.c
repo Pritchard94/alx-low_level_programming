589236c2045faaaeb80b04779c6e2d3a0d22f0c3
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-07 14:17:46 -0700

    yh

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * main - print putchar
+ *
+ * Return: return 0
+ */
+char _putchar(char c);
+
+int main(void)
+{
+	c = "_putchar";
+	int _putchar(char c)
+
+	return (0);	
+
+}
