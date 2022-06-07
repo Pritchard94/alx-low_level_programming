2409358fa59c0db9c83c5d72b87a1c648ae97fc5
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-07 16:01:22 -0700

    ty

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabe_x10 - print alphabet 10x
+ *
+ * Return: 0
+ */
+void print_alphabet_10x(void)
+{
+	char c;
+	int i;
+
+	while (i < 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		
+		i++;
+	}
+}
