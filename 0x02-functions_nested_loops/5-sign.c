0fa6f6f42049cf4ed6c1c5079158de57b807cc4f
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-07 21:13:57 -0700

    w

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - program
+ * @n: number to be checked
+ *
+ * Return: 1 for positive number, -1 for negative number 0 for anything else
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+
+		return (0);
+	}
+}
