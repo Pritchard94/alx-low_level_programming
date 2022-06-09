a4f0d3e6ae3bcf001e685833b54f9f16b610a4f1
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-09 03:44:19 -0700

    zx

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiply two numbers
+ * @x: contains number 1
+ * @y: contains number 2
+ *
+ * Return: result of multiplication
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
