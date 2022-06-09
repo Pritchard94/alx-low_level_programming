d5b55aaaf0591456513313ebfac1386615cc8d4e
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-09 03:50:44 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(1 + '0');
+	}
+
+	_putchar('\n');
+}
