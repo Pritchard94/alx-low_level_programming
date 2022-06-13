59146ebd0c9c00828cc27ff92630acaa6a2e0889
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-13 06:20:24 -0700

    putss

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _puts - print out
- * @str - string to print 
+ * @str: string to print 
  *
  * Return: 0
  */
 void _puts(char *str)
 {
-	for(; *str != '\0'; str++)
+	for (; *str != '\0'; str++)
 	{
 		_putchar(*str);
 	}
