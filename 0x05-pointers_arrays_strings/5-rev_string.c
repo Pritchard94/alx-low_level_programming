7aa10a47ca3c7b3e94207a09dacbe7669b245525
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-13 07:19:16 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * rev_string - reverses string
+ * @s: string to reverse
+ *
+ * Return: empty
+ */
+void rev_string(char *s)
+{
+	int i, c, k;
+	char *a, aux;
+
+	a = s;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (k = 0; k < c; k++)
+	{
+		a++;
+	}
+	for (i = 0; i < (c / 2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		a--;
+	}
+}
