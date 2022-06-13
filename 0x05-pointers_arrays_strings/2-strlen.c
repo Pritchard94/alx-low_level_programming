0baa3e1c179fb8d6c64c8179873be1b7bbef6447
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-13 06:08:58 -0700

    lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - Returns lenght 
+ * @s: string to count
+ *
+ * Return: lenght of string
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+	return (c);
+}
