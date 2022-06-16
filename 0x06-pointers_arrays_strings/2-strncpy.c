0a3eb401817141621c9459192f4c8e1cc43b48ca
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-16 06:53:22 -0700

    strn copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncpy - copies strings
+ * @dest: destination
+ * @src: source
+ * @n: copy limit
+ *
+ * Return: return charcarcter
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0, b = 0;
+
+	while (src[b])
+	{
+		b++;
+	}
+	while (a < n && src[a])
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+	return (dest);
+}
