9dba4626a1453974303e4510a6bb647d5a233064
Pritchard Oluwafeyisanumi <pritchardsanu@gmail.com>
2022-06-20 02:50:31 -0700

    copy mem

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - copy memory area
+ * @dest: destination
+ * @src: sosurce
+ * @n: bytes from memory area to copy
+ *
+ * Return: memory replaced
+ */
+char *_memcpy(char *dest, char *src, unsigned int c)
+{
+	unsigned int a;
+
+	for (a = 0; a < c; a++)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+}
